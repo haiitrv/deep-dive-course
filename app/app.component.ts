f60294d94b0456f083aa83461de47f26ed4df285
HaiTrinh1511 <67938967+HaiTrinh1511@users.noreply.github.com>
2024-05-24 16:15:37 +0700

    Section 1 & 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { Component } from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  courses = COURSES;
 
+  coreCourse = COURSES[0];
 
+  rxjsCourse = COURSES[1];
 
+  ngrxCourse = COURSES[2];
+
+  onCardClicked(course: Course) {
+    console.log("App component - click event bubbled...", course);
+  }
 }
