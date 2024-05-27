fe8a66312da9be3f0d6f1ddb2aab075a9a0e1ff3
HaiTrinh1511 <67938967+HaiTrinh1511@users.noreply.github.com>
2024-05-27 15:21:42 +0700

    Section 3

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,13 @@
-import { Component } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
 import { COURSES } from "../db-data";
 import { Course } from "./model/course";
+import { CourseCardComponent } from "./course-card/course-card.component";
 
 @Component({
   selector: "app-root",
@@ -18,7 +25,41 @@ export class AppComponent {
 
   course = COURSES[0];
 
+  @ViewChild("cardRef")
+  card: CourseCardComponent;
+
+  @ViewChild("containerRef")
+  containerDiv: ElementRef;
+
+  @ViewChildren(CourseCardComponent)
+  cards: QueryList<CourseCardComponent>;
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    // console.log("After View Init", this.containerDiv);
+    // console.log(this.cards.first);
+    this.cards.changes.subscribe((cards) => {
+      console.log(cards);
+    });
+  }
+
   onCardClicked(course: Course) {
     console.log("App component - click event bubbled...", course);
+    console.log("View Child", this.card);
+    console.log("Container Div", this.containerDiv);
+  }
+
+  onCoursesEdited() {
+    this.courses.push({
+      id: 1,
+      description: "Angular Core Deep Dive",
+      iconUrl:
+        "https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png",
+      longDescription:
+        "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+      category: "INTERMEDIATE",
+      lessonsCount: 10,
+    });
   }
 }
--- app/course-card/course-card.component.ts
+++ app/course-card/course-card.component.ts
@@ -10,6 +10,9 @@ export class CourseCardComponent {
   @Input()
   course: Course;
 
+  @Input()
+  cardIndex: number;
+
   @Output()
   courseSelected = new EventEmitter<Course>();
 
